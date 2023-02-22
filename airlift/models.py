289e9aed8ce91335e12220aa105a8d51be90f581
lukaskierzek <lukas.kierzek@gmail.com>
2023-02-23 00:07:14 +0100

    Modified models.py
    Add attributes for Pilots class
    Change on_delete=models.RESTRICT -> on_delete=models.CASCADE for pilot attribute in PilotsInformations class
    Change verbose name for PilotsInformations

--- airlift/models.py
+++ airlift/models.py
@@ -5,13 +5,30 @@ from django.db import models
 
 
 class Pilots(models.Model):
-    pass
+    first_name = models.CharField(
+        max_length=50,
+        help_text="Enter a pilot first name",
+        verbose_name="Pilot first name"
+    )
+
+    last_name = models.CharField(
+        max_length=50,
+        help_text="Enter a pilot last name",
+        verbose_name="Pilot last name"
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Pilot information"
+        verbose_name_plural = "Pilots informations"
 
 
 class PilotsInformations(models.Model):
     pilot = models.OneToOneField(
         to=Pilots,
-        on_delete=models.RESTRICT,
+        on_delete=models.CASCADE,
         primary_key=True
     )
 
@@ -43,5 +60,5 @@ class PilotsInformations(models.Model):
         return f"{self.position}\n{self.rank}\n{self.age}\n{self.country}"
 
     class Meta:
-        verbose_name = 'Pilot information'
-        verbose_name_plural = 'Pilots informations'
+        verbose_name = 'Pilot additional information'
+        verbose_name_plural = 'Pilot additional informations'

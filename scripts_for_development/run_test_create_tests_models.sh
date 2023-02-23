7b1889ead9cfca4db23b5adc7dec6d37be9e1e95
lukaskierzek <lukas.kierzek@gmail.com>
2023-02-23 15:02:44 +0100

    Modified run_test_create_tests_models.sh
    Change 3 command to 1

--- scripts_for_development/run_test_create_tests_models.sh
+++ scripts_for_development/run_test_create_tests_models.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 docker compose down &&
 docker compose up --build -d &&
-docker compose exec -it airlift bash -c "cd cargo_app && python manage.py makemigrations" &&
-docker compose exec -it airlift bash -c "cd cargo_app && python manage.py migrate" &&
-docker compose exec -it airlift bash -c "cd cargo_app && python manage.py test"
+docker compose exec -it airlift bash -c "cd cargo_app &&
+                                         python manage.py makemigrations &&
+                                         python manage.py migrate &&
+                                         python manage.py test"

66d7428d23089b08a91f866614c92c77713fc19a
lukaskierzek <lukas.kierzek@gmail.com>
2023-02-23 16:02:16 +0100

    Modified copy_migration.sh

--- scripts_for_development/copy_migration.sh
+++ scripts_for_development/copy_migration.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Enter a id of container: "
 read number
-docker cp ${number}:/cargo_app/cargo_app/airlift/migrations/0001_initial.py ./cargo_app/airlift/migrations/
+docker cp ${number}:/cargo_app/cargo_app/airlift/migrations/0001_initial.py ./airlift/migrations/

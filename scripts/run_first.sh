416c6b5aea1dd7f63276e583461a5592c3d43600
lukaskierzek <lukas.kierzek@gmail.com>
2023-02-23 16:02:30 +0100

    Modified run_first.sh script

--- scripts/run_first.sh
+++ scripts/run_first.sh
@@ -3,5 +3,7 @@ D_C='docker compose'
 E_A_PY_MANAGE='exec airlift python cargo_app/manage.py'
 
 $D_C up -d &&
+$D_C $E_A_PY_MANAGE makemigrations &&
+$D_C $E_A_PY_MANAGE migrate &&
 $D_C $E_A_PY_MANAGE createsuperuser
 

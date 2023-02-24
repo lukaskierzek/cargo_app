8d66109121c91e6900057db54cf7b2af0fabd509
lukaskierzek <lukas.kierzek@gmail.com>
2023-02-24 15:44:01 +0100

    Modified test_models.py
    Add dot at the end of sentences in help_text

--- airlift/tests/test_models.py
+++ airlift/tests/test_models.py
@@ -39,12 +39,12 @@ class AirliftCase(TestCase):
 
     def test_cargo_name(self):
         self.assertEqual(self.cargo._meta.get_field('name').max_length, 50)
-        self.assertEqual(self.cargo._meta.get_field('name').help_text, "Enter a cargo name")
+        self.assertEqual(self.cargo._meta.get_field('name').help_text, "Enter a cargo name.")
         self.assertEqual(self.cargo._meta.get_field('name').verbose_name, "Cargo name")
         self.assertEqual(self.cargo.name, "MGM-140 ATACMS")
 
     def test_cargo_quantity(self):
-        self.assertEqual(self.cargo._meta.get_field('quantity').help_text, "Enter a cargo quantity")
+        self.assertEqual(self.cargo._meta.get_field('quantity').help_text, "Enter a cargo quantity.")
         self.assertEqual(self.cargo._meta.get_field('quantity').verbose_name, "Cargo quantity")
         self.assertEqual(self.cargo.quantity, 1000)
 
@@ -61,7 +61,7 @@ class AirliftCase(TestCase):
         self.assertEqual(self.destination._meta.get_field('from_airport').max_length, 50)
         self.assertEqual(
             self.destination._meta.get_field('from_airport').help_text,
-            "Enter a airport name from the plane will take off"
+            "Enter a airport name from the plane will take off."
         )
         self.assertEqual(
             self.destination._meta.get_field('from_airport').verbose_name,
@@ -73,7 +73,7 @@ class AirliftCase(TestCase):
         self.assertEqual(self.destination._meta.get_field('to_airport').max_length, 50)
         self.assertEqual(
             self.destination._meta.get_field('to_airport').help_text,
-            "Enter a airport name where the plane will land"
+            "Enter a airport name where the plane will land."
         )
         self.assertEqual(
             self.destination._meta.get_field('to_airport').verbose_name,
@@ -95,7 +95,7 @@ class AirliftCase(TestCase):
 
     def test_aircraft_name(self):
         self.assertEqual(self.aircraft._meta.get_field('name').max_length, 50)
-        self.assertEqual(self.aircraft._meta.get_field('name').help_text, "Enter a aircraft name")
+        self.assertEqual(self.aircraft._meta.get_field('name').help_text, "Enter a aircraft name.")
         self.assertEqual(self.aircraft._meta.get_field('name').verbose_name, "Aircraft name")
         self.assertEqual(self.aircraft.name, "Lockheed C-130 Hercules")
 
@@ -103,7 +103,7 @@ class AirliftCase(TestCase):
         self.assertEqual(self.aircraft._meta.get_field('country').max_length, 50)
         self.assertEqual(
             self.aircraft._meta.get_field('country').help_text,
-            "Enter a country name that owns the aircraft"
+            "Enter a country name that owns the aircraft."
         )
         self.assertEqual(self.aircraft._meta.get_field('country').verbose_name, "Country name that owns the aircraft")
         self.assertEqual(self.aircraft.country, 'Poland')
@@ -122,13 +122,13 @@ class AirliftCase(TestCase):
 
     def test_pilot_first_name(self):
         self.assertEqual(self.pilot._meta.get_field('first_name').max_length, 50)
-        self.assertEqual(self.pilot._meta.get_field('first_name').help_text, "Enter a pilot first name")
+        self.assertEqual(self.pilot._meta.get_field('first_name').help_text, "Enter a pilot first name.")
         self.assertEqual(self.pilot._meta.get_field('first_name').verbose_name, "Pilot first name")
         self.assertEqual(self.pilot.first_name, "Jan")
 
     def test_pilot_last_name(self):
         self.assertEqual(self.pilot._meta.get_field('last_name').max_length, 50)
-        self.assertEqual(self.pilot._meta.get_field('last_name').help_text, "Enter a pilot last name")
+        self.assertEqual(self.pilot._meta.get_field('last_name').help_text, "Enter a pilot last name.")
         self.assertEqual(self.pilot._meta.get_field('last_name').verbose_name, "Pilot last name")
         self.assertEqual(self.pilot.last_name, "Kowalski")
 
@@ -149,25 +149,24 @@ class AirliftCase(TestCase):
 
     def test_pilot_info_position(self):
         self.assertEqual(self.pilot_info._meta.get_field('position').max_length, 50)
-        self.assertEqual(self.pilot_info._meta.get_field('position').help_text, "Enter a pilot position")
+        self.assertEqual(self.pilot_info._meta.get_field('position').help_text, "Enter a pilot position.")
         self.assertEqual(self.pilot_info._meta.get_field('position').verbose_name, "Pilot position")
         self.assertEqual(self.pilot_info.position, "First officer")
 
     def test_pilot_info_rank(self):
         self.assertEqual(self.pilot_info._meta.get_field('rank').max_length, 50)
-        self.assertEqual(self.pilot_info._meta.get_field('rank').help_text, "Enter a pilot rank")
+        self.assertEqual(self.pilot_info._meta.get_field('rank').help_text, "Enter a pilot rank.")
         self.assertEqual(self.pilot_info._meta.get_field('rank').verbose_name, "Pilot rank")
         self.assertEqual(self.pilot_info.rank, "Captain")
 
     def test_pilot_info_age(self):
-        self.assertEqual(self.pilot_info._meta.get_field('age').max_length, 50)
-        self.assertEqual(self.pilot_info._meta.get_field('age').help_text, "Enter a pilot age")
+        self.assertEqual(self.pilot_info._meta.get_field('age').help_text, "Enter a pilot age.")
         self.assertEqual(self.pilot_info._meta.get_field('age').verbose_name, "Pilot age")
         self.assertEqual(self.pilot_info.age, 35)
 
     def test_pilot_info_country(self):
         self.assertEqual(self.pilot_info._meta.get_field('country').max_length, 50)
-        self.assertEqual(self.pilot_info._meta.get_field('country').help_text, "Enter a pilot country")
+        self.assertEqual(self.pilot_info._meta.get_field('country').help_text, "Enter a pilot country.")
         self.assertEqual(self.pilot_info._meta.get_field('country').verbose_name, "Pilot country")
         self.assertEqual(self.pilot_info.country, "Poland")
 

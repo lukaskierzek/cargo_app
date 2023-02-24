29a272cdf52e646686e04953ef5dce1ea35d0615
lukaskierzek <lukas.kierzek@gmail.com>
2023-02-24 15:46:46 +0100

    Modified admin.py
    Add classes for all models

--- airlift/admin.py
+++ airlift/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Aircrafts, Destinations, Cargos, Pilots, PilotsInformations
 
-# Register your models here.
+
+@admin.register(Pilots)
+class PilotsAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+    )
+
+
+@admin.register(PilotsInformations)
+class PilotsInformationsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Aircrafts)
+class AircraftsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Destinations)
+class DestinationsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Cargos)
+class CargosAdmin(admin.ModelAdmin):
+    pass

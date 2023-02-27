830c6bb2eb6ba1fe8b31181d8f3c3ef046010323
lukaskierzek <lukas.kierzek@gmail.com>
2023-02-27 21:24:43 +0100

    Modified admin.py
    Update PilotsAdmin class
    Update PilotsInformationsAdmin class
    Update AircraftsAdmin class
    Create PilotsInformationsInline class for PilotsAdmin class

--- airlift/admin.py
+++ airlift/admin.py
@@ -2,21 +2,102 @@ from django.contrib import admin
 from .models import Aircrafts, Destinations, Cargos, Pilots, PilotsInformations
 
 
+class PilotsInformationsInline(admin.StackedInline):
+    model = PilotsInformations
+    extra = 0
+    fields = [
+        "position",
+        "rank",
+        "age",
+        "country"
+    ]
+
+
 @admin.register(Pilots)
 class PilotsAdmin(admin.ModelAdmin):
     list_display = (
         "__str__",
+        "first_name",
+        "last_name",
+        "aircraft"
     )
+
+    list_filter = (
+        "aircraft",
+    )
+
+    search_fields = (
+        "first_name",
+        "last_name",
+        "aircraft__name",
+        "aircraft__country",
+    )
+
+    search_help_text = "Serach for information by first name, last name, aircraft name or aircraft country."
+
+    inlines = (PilotsInformationsInline,)
 
 
 @admin.register(PilotsInformations)
 class PilotsInformationsAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "pilot",
+        "position",
+        "rank",
+        "age",
+        "country"
+    )
+
+    list_filter = (
+        "position",
+        "rank",
+        "country"
+    )
+
+    search_fields = (
+        "pilot__first_name",
+        "pilot__last_name",
+        "pilot__aircraft__name",
+        "pilot__age",
+        "position",
+        "rank",
+        "age",
+        "country"
+    )
+
+    search_help_text = "Serach for information by pilot or aircraft name."
 
 
 @admin.register(Aircrafts)
 class AircraftsAdmin(admin.ModelAdmin):
-    pass
+    @staticmethod
+    def gets_cargo(obj):
+        return [cargo.__str__() for cargo in obj.cargo.all()]
+
+    list_display = (
+        "__str__",
+        "country",
+        "destination",
+        "gets_cargo"
+    )
+
+    list_filter = (
+        "country",
+        "destination",
+        "destination__from_airport",
+        "destination__to_airport",
+        "cargo__name"
+    )
+
+    search_fields = (
+        "country",
+        "destination__from_airport",
+        "destination__to_airport",
+        "cargo__name",
+        "cargo__quantity"
+    )
+
+    search_help_text = "Serach for information by country, destination or cargo."
 
 
 @admin.register(Destinations)

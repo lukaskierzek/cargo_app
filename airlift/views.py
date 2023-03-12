a14d0b5622dd7f5519a18377c25de3b8d6f13744
lukaskierzek <lukas.kierzek@gmail.com>
2023-03-12 23:41:30 +0100

    Modified views.py
    Create function for index.html
    Add variables for each models

--- airlift/views.py
+++ airlift/views.py
@@ -1,3 +1,25 @@
+from django.db.models import QuerySet
 from django.shortcuts import render
+from .models import Destinations, Cargos, Pilots, Aircrafts
+
 
 # Create your views here.
+
+def index_view(request):
+    flights = Destinations.objects.all()[:5]
+    aircrafts = Aircrafts.objects.all()[:5]
+    cargos = Cargos.objects.all()[:5]
+    pilots = Pilots.objects.all()[:5]
+
+    context = {
+        'flights': flights,
+        'aircrafts': aircrafts,
+        'cargos': cargos,
+        'pilots': pilots,
+    }
+
+    return render(
+        request=request,
+        template_name='airlift/index.html',
+        context=context
+    )

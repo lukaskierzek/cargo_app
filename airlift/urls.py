e3e4c7d51764f5f17e8aaa1d4050ae00698cd981
lukaskierzek <lukas.kierzek@gmail.com>
2023-03-12 23:40:37 +0100

    Modified airlift/urls.py
    Add path for index.html

--- airlift/urls.py
+++ airlift/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
+from . import views
 
 app_name = 'airlift'
 
 urlpatterns = [
-
+    path(
+        '',
+        views.index_view,
+        name='index_view'
+    ),
+    # path(
+    #     'destination/<int:pk>',
+    #     views.DestinationsView
+    # )
 ]

d8172ce1a6391bb519cf5547f03cef75ffec8e4f
lukaskierzek <lukas.kierzek@gmail.com>
2023-03-19 19:26:06 +0100

    Modified airlift/urls.py
    Add path for cargos, pilots
    Change urlpatterns

--- airlift/urls.py
+++ airlift/urls.py
@@ -1,27 +1,74 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'airlift'
 
+# -----Main page----- #
 urlpatterns = [
     path(
         '',
         views.index_view,
         name='index_view'
     ),
+]
+
+# -----Destinations paths----- #
+urlpatterns.extend([
     path(
         'destinations/',
-        views.DestinationsListViews.as_view(),
-        name='destinations'
+        include([
+            path(
+                '',
+                views.DestinationsListViews.as_view(),
+                name='destinations'
+            ),
+            path(
+                '<int:pk>',
+                views.DestinationsDetailView.as_view(),
+                name='destinations_detail'
+            ),
+        ])
     ),
-    path(
-        'destinations/<int:pk>',
-        views.DestinationsDetailView.as_view(),
-        name='destinations_detail'
-    ),
+])
+
+# -----Aircrafts paths----- #
+urlpatterns.extend([
     path(
         'aircrafts/',
-        views.AircraftsListViews.as_view(),
-        name='aircrafts'
-    )
-]
+        include([
+            path(
+                '',
+                views.AircraftsListViews.as_view(),
+                name='aircrafts'
+            ),
+        ])
+    ),
+])
+
+# -----Pilots paths----- #
+urlpatterns.extend([
+    path(
+        'pilots/',
+        include([
+            path(
+                '',
+                views.PilotsListViews.as_view(),
+                name='pilots'
+            ),
+        ])
+    ),
+])
+
+# -----Cargos paths----- #
+urlpatterns.extend([
+    path(
+        'cargos/',
+        include([
+            path(
+                '',
+                views.CargosListViews.as_view(),
+                name='cargos'
+            ),
+        ])
+    ),
+])

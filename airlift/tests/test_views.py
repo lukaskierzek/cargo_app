9226f91acde3b970fff5ec6624c903b16b536cfa
lukaskierzek <lukas.kierzek@gmail.com>
2023-03-19 19:24:52 +0100

    Updated test_views.py
    Add tests for pilots, cargos and aircrafts

--- airlift/tests/test_views.py
+++ airlift/tests/test_views.py
@@ -6,9 +6,13 @@ from airlift.models import Destinations
 class ViewsTest(TestCase):
     fixtures = ['airlift_fixture.json']
 
+    # ----------tests for main page---------- #
+
     def test_get_index_view(self):
         response = self.client.get(reverse('airlift:index_view'))
         self.assertEqual(response.status_code, 200)
+
+    # ----------tests for Destinations---------- #
 
     def test_get_destination_detail(self):
         response = self.client.get(reverse('airlift:destinations_detail', kwargs={'pk': 1}))
@@ -18,6 +22,32 @@ class ViewsTest(TestCase):
         response = self.client.get(reverse('airlift:destinations'))
         self.assertEqual(response.status_code, 200)
 
+    # ----------tests for Aircrafts---------- #
+
+    def test_get_aircraft_detail(self):
+        response = self.client.get(reverse('airlift:aircrafts_detail', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
+
     def test_get_aircrafts_list(self):
         response = self.client.get(reverse('airlift:aircrafts'))
+        self.assertEqual(response.status_code, 200)
+
+    # ----------tests for Pilots---------- #
+
+    def test_get_pilot_detail(self):
+        response = self.client.get(reverse('airlift:pilots_detail', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_pilots_list(self):
+        response = self.client.get(reverse('airlift:pilots'))
+        self.assertEqual(response.status_code, 200)
+
+    # ----------tests for Cargos---------- #
+
+    def test_get_cargo_detail(self):
+        response = self.client.get(reverse('airlift:cargos_detail', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_cargos_list(self):
+        response = self.client.get(reverse('airlift:cargos'))
         self.assertEqual(response.status_code, 200)

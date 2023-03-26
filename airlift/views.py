eb141b98a4f502a70c9710c5470409e32e4a7dd9
lukaskierzek <lukas.kierzek@gmail.com>
2023-03-26 23:52:05 +0200

    Modified airlift/views.py
    Add classes for cargo detail, pilot detail, aircraft detail
    Add login required for index_view function
    Add login required for all classes

--- airlift/views.py
+++ airlift/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 from .models import Destinations, Cargos, Pilots, Aircrafts
 
 
 # Create your views here.
 
+@login_required
 def index_view(request):
     flights = Destinations.objects.all()[:4]
     aircrafts = Aircrafts.objects.all()[:4]
@@ -26,31 +29,49 @@ def index_view(request):
     )
 
 
-class DestinationsDetailView(DetailView):
+class DestinationsDetailView(LoginRequiredMixin, DetailView):
     model = Destinations
     template_name = 'airlift/destinations_detail.html'
     context_object_name = 'flights'
 
 
-class DestinationsListViews(ListView):
+class DestinationsListViews(LoginRequiredMixin, ListView):
     model = Destinations
     template_name = 'airlift/destinations.html'
     context_object_name = 'flights'
 
 
-class AircraftsListViews(ListView):
+class AircraftsDetailView(LoginRequiredMixin, DetailView):
+    model = Aircrafts
+    template_name = 'airlift/aircrafts_detail.html'
+    context_object_name = 'aircraft'
+
+
+class AircraftsListViews(LoginRequiredMixin, ListView):
     model = Aircrafts
     template_name = 'airlift/aircrafts.html'
     context_object_name = 'aircrafts'
 
 
-class PilotsListViews(ListView):
+class PilotsDetailView(LoginRequiredMixin, DetailView):
+    model = Pilots
+    template_name = 'airlift/pilots_detail.html'
+    context_object_name = 'pilot'
+
+
+class PilotsListViews(LoginRequiredMixin, ListView):
     model = Pilots
     template_name = 'airlift/pilots.html'
     context_object_name = 'pilots'
 
 
-class CargosListViews(ListView):
+class CargosDetailViews(LoginRequiredMixin, DetailView):
+    model = Cargos
+    template_name = 'airlift/cargos_detail.html'
+    context_object_name = 'cargo'
+
+
+class CargosListViews(LoginRequiredMixin, ListView):
     model = Cargos
     template_name = 'airlift/cargos.html'
     context_object_name = 'cargos'

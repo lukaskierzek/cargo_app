dbfe24d144caf2b9a50bfb8a309e98c07a78a98f
lukaskierzek <lukas.kierzek@gmail.com>
2023-03-29 20:58:55 +0200

    Modified airlift/tests/test_views.py
    Add follow=true for all funtion

--- airlift/tests/test_views.py
+++ airlift/tests/test_views.py
@@ -9,45 +9,45 @@ class ViewsTest(TestCase):
     # ----------tests for main page---------- #
 
     def test_get_index_view(self):
-        response = self.client.get(reverse('airlift:index_view'))
+        response = self.client.get(reverse('airlift:index_view'), follow=True)
         self.assertEqual(response.status_code, 200)
 
     # ----------tests for Destinations---------- #
 
     def test_get_destination_detail(self):
-        response = self.client.get(reverse('airlift:destinations_detail', kwargs={'pk': 1}))
+        response = self.client.get(reverse('airlift:destinations_detail', kwargs={'pk': 1}), follow=True)
         self.assertEqual(response.status_code, 200)
 
     def test_get_destinations_list(self):
-        response = self.client.get(reverse('airlift:destinations'))
+        response = self.client.get(reverse('airlift:destinations'), follow=True)
         self.assertEqual(response.status_code, 200)
 
     # ----------tests for Aircrafts---------- #
 
     def test_get_aircraft_detail(self):
-        response = self.client.get(reverse('airlift:aircrafts_detail', kwargs={'pk': 1}))
+        response = self.client.get(reverse('airlift:aircrafts_detail', kwargs={'pk': 1}), follow=True)
         self.assertEqual(response.status_code, 200)
 
     def test_get_aircrafts_list(self):
-        response = self.client.get(reverse('airlift:aircrafts'))
+        response = self.client.get(reverse('airlift:aircrafts'), follow=True)
         self.assertEqual(response.status_code, 200)
 
     # ----------tests for Pilots---------- #
 
     def test_get_pilot_detail(self):
-        response = self.client.get(reverse('airlift:pilots_detail', kwargs={'pk': 1}))
+        response = self.client.get(reverse('airlift:pilots_detail', kwargs={'pk': 1}), follow=True)
         self.assertEqual(response.status_code, 200)
 
     def test_get_pilots_list(self):
-        response = self.client.get(reverse('airlift:pilots'))
+        response = self.client.get(reverse('airlift:pilots'), follow=True)
         self.assertEqual(response.status_code, 200)
 
     # ----------tests for Cargos---------- #
 
     def test_get_cargo_detail(self):
-        response = self.client.get(reverse('airlift:cargos_detail', kwargs={'pk': 1}))
+        response = self.client.get(reverse('airlift:cargos_detail', kwargs={'pk': 1}), follow=True)
         self.assertEqual(response.status_code, 200)
 
     def test_get_cargos_list(self):
-        response = self.client.get(reverse('airlift:cargos'))
+        response = self.client.get(reverse('airlift:cargos'), follow=True)
         self.assertEqual(response.status_code, 200)

f7ea67c0c3bbc0cf07ca1abc29e11c1391e681b2
lukaskierzek <lukas.kierzek@gmail.com>
2023-03-30 14:05:31 +0200

    Update settings.py
    Add airlift_api directory and rest framework to installed apps

--- cargo_app/settings.py
+++ cargo_app/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'airlift.apps.AirliftConfig',
+    'airlift_api.apps.AirliftApiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [

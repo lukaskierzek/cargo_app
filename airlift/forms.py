fdf9414489928f7bc6dc83011c41650175f6bd37
lukaskierzek <lukas.kierzek@gmail.com>
2023-04-19 19:44:07 +0200

    Create airlift forms.py
    Create form class for Destinations, Aircraft, Cargos and PilotsInformations

---
+++ airlift/forms.py
@@ -0,0 +1,70 @@
+from django import forms
+
+from .models import Destinations, Aircrafts, Cargos, PilotsInformations
+
+
+class DestinationUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Destinations
+        fields = (
+            'from_airport',
+            'to_airport',
+            'scheduled_take_off',
+            'scheduled_landing',
+            'comment'
+        )
+        widgets = {
+            'from_airport': forms.TextInput(attrs={'size': 60}),
+            'to_airport': forms.TextInput(attrs={'size': 60}),
+            'scheduled_take_off': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'scheduled_landing': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'comment': forms.Textarea(attrs={'cols': 60, 'rows': 10})
+        }
+
+
+class AircraftUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Aircrafts
+        fields = (
+            'name',
+            'country',
+            'destination',
+            'cargo',
+            'comment'
+        )
+        widgets = {
+            'name': forms.TextInput(attrs={'size': 60}),
+            'country': forms.TextInput(attrs={'size': 60}),
+            'cargo': forms.SelectMultiple(attrs={'size': 10, }),
+            'comment': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+        }
+
+
+class CargoUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Cargos
+        fields = (
+            'name',
+            'quantity',
+            'comment',
+        )
+        widgets = {
+            'name': forms.TextInput(attrs={'size': 60}),
+            'comment': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+        }
+
+
+class PilotsInformationsUpdateForm(forms.ModelForm):
+    class Meta:
+        model = PilotsInformations
+        fields = (
+            'pilot',
+            'position',
+            'rank',
+            'age',
+            'country',
+            'comment',
+        )
+        widgets = {
+            'comment': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+        }

924bf3c362e338b0ff193cda16a6a8262e0156d2
lukaskierzek <lukas.kierzek@gmail.com>
2023-10-05 14:18:11 +0200

    Update airlift views.py
    Add rows_limit variable in index_view function
    Change path for templates

--- airlift/views.py
+++ airlift/views.py
@@ -11,10 +11,12 @@ from .models import Destinations, Cargos, Pilots, Aircrafts, PilotsInformations
 
 @login_required
 def index_view(request):
-    flights = Destinations.objects.all()[:4]
-    aircrafts = Aircrafts.objects.all()[:4]
-    cargos = Cargos.objects.all()[:4]
-    pilots = Pilots.objects.all()[:4]
+    rows_limit: int = 4
+
+    flights = Destinations.objects.all()[:rows_limit]
+    aircrafts = Aircrafts.objects.all()[:rows_limit]
+    cargos = Cargos.objects.all()[:rows_limit]
+    pilots = Pilots.objects.all()[:rows_limit]
 
     context = {
         'flights': flights,
@@ -32,82 +34,82 @@ def index_view(request):
 
 class DestinationsDetailView(LoginRequiredMixin, DetailView):
     model = Destinations
-    template_name = 'airlift/destinations_detail.html'
+    template_name = 'airlift/destinations/destinations_detail.html'
     context_object_name = 'flight'
 
 
 class DestinationsListViews(LoginRequiredMixin, ListView):
     model = Destinations
-    template_name = 'airlift/destinations.html'
+    template_name = 'airlift/destinations/destinations.html'
     context_object_name = 'flights'
 
 
 class DestinationsUpdate(LoginRequiredMixin, UpdateView):
     model = Destinations
-    template_name = 'airlift/destinations_form.html'
+    template_name = 'airlift/destinations/destinations_form.html'
     context_object_name = 'flight'
     form_class = DestinationUpdateForm
 
 
 class AircraftsDetailView(LoginRequiredMixin, DetailView):
     model = Aircrafts
-    template_name = 'airlift/aircrafts_detail.html'
+    template_name = 'airlift/aircrafts/aircrafts_detail.html'
     context_object_name = 'aircraft'
 
 
 class AircraftsListViews(LoginRequiredMixin, ListView):
     model = Aircrafts
-    template_name = 'airlift/aircrafts.html'
+    template_name = 'airlift/aircrafts/aircrafts.html'
     context_object_name = 'aircrafts'
 
 
 class AircraftsUpdate(LoginRequiredMixin, UpdateView):
     model = Aircrafts
-    template_name = 'airlift/aircrafts_form.html'
+    template_name = 'airlift/aircrafts/aircrafts_form.html'
     context_object_name = 'aircraft'
     form_class = AircraftUpdateForm
 
 
 class PilotsDetailView(LoginRequiredMixin, DetailView):
     model = Pilots
-    template_name = 'airlift/pilots_detail.html'
+    template_name = 'airlift/pilots/pilots_detail.html'
     context_object_name = 'pilot'
 
 
 class PilotsListViews(LoginRequiredMixin, ListView):
     model = Pilots
-    template_name = 'airlift/pilots.html'
+    template_name = 'airlift/pilots/pilots.html'
     context_object_name = 'pilots'
 
 
 class PilotsUpdate(LoginRequiredMixin, UpdateView):
     model = Pilots
     fields = '__all__'
-    template_name = 'airlift/pilots_form.html'
+    template_name = 'airlift/pilots/pilots_form.html'
     context_object_name = 'pilot'
 
 
 class PilotsInformationsUpdate(LoginRequiredMixin, UpdateView):
     model = PilotsInformations
-    template_name = 'airlift/pilots_informations_form.html'
+    template_name = 'airlift/pilots/pilots_informations_form.html'
     context_object_name = 'pilots_informations'
     form_class = PilotsInformationsUpdateForm
 
 
 class CargosDetailViews(LoginRequiredMixin, DetailView):
     model = Cargos
-    template_name = 'airlift/cargos_detail.html'
+    template_name = 'airlift/cargos/cargos_detail.html'
     context_object_name = 'cargo'
 
 
 class CargosListViews(LoginRequiredMixin, ListView):
     model = Cargos
-    template_name = 'airlift/cargos.html'
+    template_name = 'airlift/cargos/cargos.html'
     context_object_name = 'cargos'
 
 
 class CargosUpdate(LoginRequiredMixin, UpdateView):
     model = Cargos
-    template_name = 'airlift/cargos_form.html'
+    template_name = 'airlift/cargos/cargos_form.html'
     context_object_name = 'cargo'
     form_class = CargoUpdateForm
